e78ae5611ea48e5e9a25ae30de5959c6200d0dbb
caleb <calebburke91@gmail.com>
2024-01-06 12:28:13 -0800

    reworking dir

--- app.cpp
+++ app.cpp
@@ -16,9 +16,7 @@
 
 int main() {
     glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwTerminate();
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -28,15 +26,8 @@ int main() {
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
+	
+	std::cout << __FILE__ << __LINE__ << '\n';
     return 0;
 }
 
