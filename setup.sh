dd9cbccee4bd6f2f82578eb86730c33c4b743537
caleb <calebburke91@gmail.com>
2024-12-13 01:14:18 -0800

    Moved all code to legacy
    To be removed or refactored

--- setup.sh
+++ setup.sh
@@ -23,3 +23,24 @@ yon "Append to LD_LIBRARY_PATH?" && {
 	printenv LD_LIBRARY_PATH
 }
 
+# env
+ADD_PATH="export LD_LIBRARY_PATH="$PWD/engine/build/lib/:$LD_LIBRARY_PATH""
+REMOVE_PATH="export LD_LIBRARY_PATH="""
+
+echo $ADD_PATH
+eval "$ADD_PATH"
+
+# Yes or no func 
+yon(){
+	read -p "$* (Y/n): " -e res
+	res=${res,,}
+	case $res in
+		'' | [Yy]*) return 0 ;;
+		*) return 1 ;;
+	esac
+}
+
+yon "Clear LD_LIBRARY_PATH?" && {
+	echo $REMOVE_PATH
+	eval "$REMOVE_PATH"
+}
