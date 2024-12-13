73a18a3967fab3f2495fbba9a6721e666229b0b5
caleb <calebburke91@gmail.com>
2024-12-13 01:24:20 -0800

    dummy code for starting build env

---
+++ app.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author Caleb Burke
+ * @date Dec 13, 2024
+ */
+
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, const char** argv) {
+	(void)argc;
+	(void)argv;
+	std::cout << __FILE__ << "::" << __LINE__ << '\n';
+
+	return EXIT_SUCCESS;
+}
