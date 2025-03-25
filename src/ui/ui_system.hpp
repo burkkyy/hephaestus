bfe54fc4dcb38679c3e9f49dee440b999a554e65
caleb <calebburke91@gmail.com>
2025-03-25 03:18:51 -0700

    ui system (simular to RenderSystem)

---
+++ src/ui/ui_system.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <imgui.h>
+
+#include <memory>
+#include <vector>
+
+#include "device.hpp"
+#include "frame_info.hpp"
+#include "renderer.hpp"
+#include "ui_component.hpp"
+#include "window.hpp"
+
+namespace hep {
+
+class UISystem {
+ public:
+  UISystem(Window& window, Device& device, Renderer& renderer,
+           vk::DescriptorPool& descriptorPool);
+  ~UISystem();
+
+  void setup();
+  void update(const FrameInfo& frameInfo);
+  void render(vk::CommandBuffer commandBuffer);
+
+  void addComponent(std::unique_ptr<UIComponent> component);
+
+ private:
+  bool showDemoWindow = true;
+
+  std::vector<std::unique_ptr<UIComponent>> components;
+};
+
+}  // namespace hep
